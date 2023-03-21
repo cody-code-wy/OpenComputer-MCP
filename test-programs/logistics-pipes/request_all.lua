9ac9294484179e6aabb49e23c8557991c27ad8e3
Cody Young <william@werl.me>
2023-03-20 23:03:30 -0500

    Add debug statement

--- test-programs/logistics-pipes/request_all.lua
+++ test-programs/logistics-pipes/request_all.lua
@@ -1,5 +1,6 @@
 local pipe = component.logisticspipe.getPipe()
 local items = pipe.getAvailableItems()
+print("Requesting all items I can find... this might take up a lot of storage space!")
 for _,item in pairs(items) do
   local itemStack = item.getValue1()
   print("Requesting :" .. item.getValue2() .. " of " .. itemStack.getName() .. " from mod " .. itemStack.getModName())
