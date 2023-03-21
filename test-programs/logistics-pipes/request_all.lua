5ba3e310d0c4011ae393ed962df73692caa396db
Cody Young <william@werl.me>
2023-03-20 20:42:52 -0500

    Add logistics pipes test

---
+++ test-programs/logistics-pipes/request_all.lua
@@ -0,0 +1,7 @@
+local pipe = component.logisticspipe.getPipe()
+local items = pipe.getAvailableItems()
+for _,item in pairs(items) do
+  local itemStack = item.getValue1()
+  print("Requesting :" .. item.getValue2() .. " of " .. itemStack.getName() .. " from mod " .. itemStack.getModName())
+  pipe.makeRequest(itemStack, item.getValue2() + .0)
+end
